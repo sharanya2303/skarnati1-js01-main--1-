6f2ec4014732f3a93879a3ba07c8eed7929565db
sharanya <sharanyak2303@gmail.com>
2024-08-31 03:03:21 +0530

    commit

---
+++ skarnati1-js01-main/project02/project01-02.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Sharanya Karnati
+    Date:   08/28/24  
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+service1Name = "Basic", service2Name = "Express", service3Name = "Extreme",
+service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed ="100Mbps", 
+service3Speed = "500 Mbps",  service4Speed = "1 Gig"
---
+++ skarnati1-js01-main/project04/project01-04.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: 
+    Date:   
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+let homeStreet ="1Main St."
+let homeCity ="Sicilia"
+let homeState ="MA"
+let homeCode = "20034"
+let workStreet ="15Oak Ln."
+let workCity ="Central City"
+let workState ="MA"
+let workCode ="02104"
